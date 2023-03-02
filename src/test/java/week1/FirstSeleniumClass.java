33b2dda12ddf83e679b273fd31b03032a15d95de
Guzel <kz_guzel@yahoo.com>
2023-03-02 13:32:53 +0000

    selenium review

---
+++ src/test/java/week1/FirstSeleniumClass.java
@@ -0,0 +1,22 @@
+package week1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class FirstSeleniumClass {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup(); //1. step
+        WebDriverManager.edgedriver().setup();
+
+        WebDriver driverChrome = new ChromeDriver(); // 2 .step
+
+        driverChrome.get("https://practice.cydeo.com/");
+
+        WebDriver driverEdge = new EdgeDriver();
+
+        driverEdge.get("https://www.google.com");
+    }
+}
